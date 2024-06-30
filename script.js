0903f5cbb74142beb8186b1b23be08d84a067f2a
staboma <163428917+staboma@users.noreply.github.com>
2024-06-30 23:10:11 +0300

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// script.js
 document.getElementById('bmiForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
@@ -11,12 +10,16 @@ document.getElementById('bmiForm').addEventListener('submit', function(event) {
 
     if (bmi < 18.5) {
         message = 'Zayıf';
+        document.getElementById('result').style.color = '#ffc107';
     } else if (bmi < 24.9) {
         message = 'Normal';
+        document.getElementById('result').style.color = '#28a745';
     } else if (bmi < 29.9) {
         message = 'Fazla Kilolu';
+        document.getElementById('result').style.color = '#ff9800';
     } else {
         message = 'Obez';
+        document.getElementById('result').style.color = '#dc3545';
     }
 
     document.getElementById('result').innerText = `BMI: ${bmi} - ${message}`;
